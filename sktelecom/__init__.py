2a72a30eaf8a1088e2fb5b44a5e18b5431038b2b
Juan <jabaldonedo@gmail.com>
2014-01-01 15:28:07 +0100

    defining  main package

---
+++ sktelecom/__init__.py
@@ -0,0 +1,9 @@
+"""
+Telecommunications module for Python
+====================================
+
+sktelecom is a Python module providing tools and algorithms for the design,
+simulation, and analysis of communications systems in the tightly-knit world of scientific Python
+packages (numpy, scipy, matplotlib).
+
+"""
