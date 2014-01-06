4959f37497cfbd5dea183f381d6f7d0670115e3a
Juan <jabaldonedo@gmail.com>
2014-01-06 17:27:05 +0100

    Brester test for isotropic dielectric case

--- sktelecom/multilayer/tests/test_multilayer.py
+++ sktelecom/multilayer/tests/test_multilayer.py
@@ -1,14 +1,12 @@
-
-from numpy.testing import assert_approx_equal
+import numpy as np
+from numpy.testing import assert_array_almost_equal
 from sktelecom import multilayer
 
 
 def test_brewster():
-    n1 = 1.2
-    n2 = 1.5
+    n1 = 1.5
+    n2 = 1
 
     thb, thc = multilayer.brewster(n1, n2)
 
-    assert_approx_equal(thb, 51.3402, significant=5)
-    assert_approx_equal(thc, 53.1301, significant=5)
-
+    assert_array_almost_equal(np.array([thb, thc]), np.array([33.6901, 41.8103]), decimal=4)
