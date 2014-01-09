4d254ffcecbe87033da9fcf1321e13c105e3808f
Juan <jabaldonedo@gmail.com>
2014-01-09 19:10:01 +0100

    Adding Brewster test cases

--- sktelecom/multilayer/tests/test_multilayer.py
+++ sktelecom/multilayer/tests/test_multilayer.py
@@ -19,3 +19,21 @@ def test_brewster_air2glass():
     thb, thc, _ = multilayer.brewster(n1, n2)
 
     assert_array_almost_equal(np.array([thb, thc]), np.array([56.3099, 41.8103]), decimal=4)
+
+
+def test_brewster_birefringent_uniaxial():
+    n1 = np.array([1.1, 1.2])
+    n2 = [1, 1]
+
+    thb, thc_te, thc_tm = multilayer.brewster(n1, n2)
+
+    assert_array_almost_equal(np.array([thb, thc_te, thc_tm]), np.array([34.4166, 65.3800, 58.6984]), decimal=4)
+
+
+def test_brewster_birefringent_biaxial():
+    n1 = [1.1, 1.2, 1.4]
+    n2 = [1.5, 1.5, 1.5]
+
+    thb, thc_te, thc_tm = multilayer.brewster(n1, n2)
+
+    assert_array_almost_equal(np.array([thb, thc_te, thc_tm]), np.array([73.0770, 53.1301, 68.9605]), decimal=4)
