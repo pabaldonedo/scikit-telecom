42f76e55c6ada5e90ba6025ca0db559432447cb0
Juan <jabaldonedo@gmail.com>
2014-01-12 19:10:01 +0100

    Adding tests for fresnel function

--- sktelecom/multilayer/tests/test_multilayer.py
+++ sktelecom/multilayer/tests/test_multilayer.py
@@ -37,3 +37,51 @@ def test_brewster_birefringent_biaxial():
     thb, thc_te, thc_tm = multilayer.brewster(n1, n2)
 
     assert_array_almost_equal(np.array([thb, thc_te, thc_tm]), np.array([73.0770, 53.1301, 68.9605]), decimal=4)
+
+
+def test_fresnel_air2glass_30deg():
+    n1 = 1
+    n2 = 1.5
+    theta = 30
+
+    rte, rtm = multilayer.fresnel(n1, n2, theta)
+
+    assert_array_almost_equal(np.array([rte, rtm]), np.array([-0.2404, -0.1589]), decimal=4)
+
+
+def test_fresnel_glass2air_30deg():
+    n1 = 1.5
+    n2 = 1
+    theta = 30
+
+    rte, rtm = multilayer.fresnel(n1, n2, theta)
+
+    assert_array_almost_equal(np.array([rte, rtm]), np.array([0.3252, 0.0679]), decimal=4)
+
+
+def test_fresnel_air2glass_0_to_90deg():
+    n1 = 1
+    n2 = 1.5
+    theta = np.arange(100, step=10, dtype=np.int)
+
+    rte, rtm = multilayer.fresnel(n1, n2, theta)
+
+    rte_test = np.array([-0.2000, -0.2041, -0.2170, -0.2404, -0.2778, -0.3347, -0.4202, -0.5474, -0.7339, -1.0000])
+    rtm_test = np.array([-0.2000, -0.1959, -0.1829, -0.1589, -0.1196, -0.0572, 0.0424, 0.2061, 0.4866, 1.0000])
+
+    assert_array_almost_equal(rte, rte_test, decimal=4)
+    assert_array_almost_equal(rtm, rtm_test, decimal=4)
+
+
+def test_fresnel_birefringent_biaxial_mediums_0_to_90deg():
+    n1 = [1.2, 1.1, 1.3]
+    n2 = [1, 1.5, 1.3]
+    theta = np.arange(100, step=10, dtype=np.int)
+
+    rte, rtm = multilayer.fresnel(n1, n2, theta)
+
+    rte_test = np.array([-0.1538, -0.1573, -0.1683, -0.1886, -0.2218, -0.2741, -0.3562, -0.4858, -0.6890, -1.0000])
+    rtm_test = np.array([0.0909, 0.0909, 0.0909, 0.0909, 0.0909, 0.0909, 0.0909, 0.0909, 0.0909, 0.0909])
+
+    assert_array_almost_equal(rte, rte_test, decimal=4)
+    assert_array_almost_equal(rtm, rtm_test, decimal=4)
