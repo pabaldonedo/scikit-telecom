df5f46c84e687ec70843a3a7a60de8b75254b7d8
Juan <jabaldonedo@gmail.com>
2014-02-10 11:45:17 +0100

    new test case for checking wavelength in plane waves

--- sktelecom/waves/tests/test_plane_wave.py
+++ sktelecom/waves/tests/test_plane_wave.py
@@ -12,3 +12,13 @@ def test_plane_wave_frequency():
     freq = p.frequency()
 
     assert_approx_equal(freq, 101929435.72)
+
+
+def test_plane_wave_wavelength():
+    a = np.array([-1, 0, 1j])
+    k = np.array([0, -0.68j * np.pi, 0])
+
+    p = waves.PlaneWave(a, k)
+    wave_len = p.wavelength()
+
+    assert_approx_equal(wave_len, 2.9411764705882346)
