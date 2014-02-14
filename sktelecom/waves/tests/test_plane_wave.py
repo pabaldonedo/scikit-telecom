761ad82020808c54177696625c384456e397b865
Juan <jabaldonedo@gmail.com>
2014-02-14 13:15:04 +0100

    new test case for testing circular decomposition of a plane wave

--- sktelecom/waves/tests/test_plane_wave.py
+++ sktelecom/waves/tests/test_plane_wave.py
@@ -32,6 +32,19 @@ def test_plane_decompose_linear():
     al1, al2, u1, u2 = p.decompose_linear()
 
     assert_array_almost_equal(al1, -20j)
-    assert_array_almost_equal(al2, -8.66025403784+0j)
-    assert_array_almost_equal(u1,  np.array([1, 0, 0]))
+    assert_array_almost_equal(al2, -8.66025403784 + 0j)
+    assert_array_almost_equal(u1, np.array([1, 0, 0]))
     assert_array_almost_equal(u2, np.array([0, 0.5, -0.8660254]))
+
+
+def test_plane_decompose_circular():
+    a = np.array([-20j, 5 * np.sqrt(3), 15])
+    k = np.array([0, np.sqrt(3), 1]) * 30j * np.pi
+
+    p = waves.PlaneWave(a, k)
+    ac1, ac2, v1, v2 = p.decompose_circular()
+
+    assert_array_almost_equal(ac1, -5.66987298108j)
+    assert_array_almost_equal(ac2, -14.3301270189j)
+    assert_array_almost_equal(v1, np.array([0.70710678 + 0.j, 0 + 0.35355339j, 0 - 0.61237244j]))
+    assert_array_almost_equal(v2, np.array([0.70710678 + 0.j, 0 - 0.35355339j, 0 + 0.61237244j]))
